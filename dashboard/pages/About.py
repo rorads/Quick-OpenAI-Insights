31bac76ef53f60ff6482294ec7704dd546c2770f
Rory Scott <Rory.Scott@PACONSULTING.COM>
2023-03-11 09:07:47 +0000

    restructure: seperate dasbhoard dir and multipage

---
+++ dashboard/pages/About.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# Adding markdown text using a magic command, eabled in the config.toml file
+"""
+# About this app
+
+This app is a work in progress. So far the following features have been implemented:
+
+- Manually input the transcript of a youtube video
+- Preprocess the the transcript into chunks of 10 lines and store in a dataframe, serialised as a json file
+- Run NLP analysis on the text file using OpenAI's GPT-3 API
+- Analyse and visualise the results using Streamlit
+
+Please get in touch via Github if you have any questions or suggestions, or contact my via my work email address (rory dot scott at paconsulting dot com).
+
+"""
